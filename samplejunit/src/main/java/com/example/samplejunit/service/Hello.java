cc6fe6338769487006bae67cf5451b0b1cd1b133
deepakkumar18 <deepak1987rajput@gmail.com>
2022-08-31 18:00:44 +0530

    Asserting Null and Non Null Values

--- samplejunit/src/main/java/com/example/samplejunit/service/Hello.java
+++ samplejunit/src/main/java/com/example/samplejunit/service/Hello.java
@@ -4,4 +4,11 @@ public class Hello {
 	public int calculate(int a, int b) {
 		return a + b;
 	}
+	
+	public Object checkNull(Object obj) {
+		if(obj !=null) {
+			return obj;
+		}
+		return null;
+	}
 }
--- samplejunit/src/test/java/com/example/samplejunit/service/HelloTest.java
+++ samplejunit/src/test/java/com/example/samplejunit/service/HelloTest.java
@@ -18,4 +18,15 @@ public class HelloTest {
 		int result = hello.calculate(2, 3);
 		Assertions.assertNotEquals(7,result," 2 + 3 should not be 7");
 	}
+	
+	@Test
+	public void checkNull() {
+		String s = new String("Deepak");
+		String s1 = null;
+		Object obj = hello.checkNull(s);
+		
+		Assertions.assertNotNull(obj,"Object should not be null");
+		Assertions.assertNull(s1,"Object should be null");
+		
+	}
 }
