ac8397ef0435c9e6157584b0afeb2dcc01e6c0c2
deepakkumar18 <deepak1987rajput@gmail.com>
2022-08-31 18:16:00 +0530

    Added @BeforeEach annotation

--- samplejunit/src/test/java/com/example/samplejunit/service/HelloTest.java
+++ samplejunit/src/test/java/com/example/samplejunit/service/HelloTest.java
@@ -1,26 +1,38 @@
 package com.example.samplejunit.service;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class HelloTest {
 
-	private Hello hello =  new Hello(); 
+	private Hello hello;
+	
+	@BeforeEach
+	public void init() {
+		System.out.println();
+		System.out.println("before each got printed");
+		
+		hello =  new Hello(); 
+	}
 	
 	@Test
 	public void test() {
+		System.out.println("You are in method test");
 		int result = hello.calculate(2, 3);
 		Assertions.assertEquals(5,result," 2 + 3 should be 5");
 	}
 	
 	@Test
 	public void testTwo() {
+		System.out.println("You are in method testTwo");
 		int result = hello.calculate(2, 3);
 		Assertions.assertNotEquals(7,result," 2 + 3 should not be 7");
 	}
 	
 	@Test
 	public void checkNull() {
+		System.out.println("You are in method checkNull");
 		String s = new String("Deepak");
 		String s1 = null;
 		Object obj = hello.checkNull(s);
