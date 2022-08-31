e653b24dbc0782ba45559b8bf0b76ab67f5a1384
deepakkumar18 <deepak1987rajput@gmail.com>
2022-08-31 13:34:21 +0530

    first commit

---
+++ samplejunit/src/main/java/com/example/samplejunit/SamplejunitApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplejunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplejunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplejunitApplication.class, args);
+	}
+
+}
---
+++ samplejunit/src/test/java/com/example/samplejunit/SamplejunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplejunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplejunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
