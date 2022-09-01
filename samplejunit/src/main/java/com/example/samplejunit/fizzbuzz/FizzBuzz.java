16e472d2c203267e251de6ec4cd81926f7521a5c
deepakkumar18 <deepak1987rajput@gmail.com>
2022-09-01 22:48:01 +0530

    Added Parameterized test

--- samplejunit/src/main/java/com/example/samplejunit/fizzbuzz/FizzBuzz.java
+++ samplejunit/src/main/java/com/example/samplejunit/fizzbuzz/FizzBuzz.java
@@ -3,11 +3,14 @@ package com.example.samplejunit.fizzbuzz;
 public class FizzBuzz {
 
 	public String compute(int i) {
-		if(i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+			return "fizzbuzz";
+		else if (i % 3 == 0)
 			return "fizz";
-		
-		
-		return null;
+		else if (i % 5 == 0)
+			return "buzz";
+		else
+			return Integer.toString(i);
 	}
 
 }
--- samplejunit/src/test/java/com/example/samplejunit/fizzbuzz/FizzBuzzTest.java
+++ samplejunit/src/test/java/com/example/samplejunit/fizzbuzz/FizzBuzzTest.java
@@ -3,23 +3,52 @@ package com.example.samplejunit.fizzbuzz;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class FizzBuzzTest {
-	
+
 	private FizzBuzz fizzBuzz;
-	
+
 	@BeforeEach
-	public void initialize()
-	{
+	public void initialize() {
 		fizzBuzz = new FizzBuzz();
 	}
-	
+
 	@Test
 	public void divisibleByThree() {
 		String expected = "fizz";
-		
 		Assertions.assertEquals(expected, fizzBuzz.compute(3));
-		
+
+	}
+
+	@Test
+	public void divisibleByFive() {
+		String expected = "buzz";
+		Assertions.assertEquals(expected, fizzBuzz.compute(5));
+
+	}
+
+	@Test
+	public void divisibleByThreeAndFive() {
+		String expected = "fizzbuzz";
+		Assertions.assertEquals(expected, fizzBuzz.compute(15));
+	}
+
+	@Test
+	public void notDivisibleByThreeOrFive() {
+		String expected = "1";
+		Assertions.assertEquals(expected, fizzBuzz.compute(1));
+	}
+	/*
+	 * @Test public void myTest() { for(int i = 1; i<=16; i++)
+	 * System.out.println(fizzBuzz.compute(i)); }
+	 */
+
+	@ParameterizedTest(name = "value={0}, expected={1}")
+	@CsvFileSource(resources = "/small-test-data.csv")
+	public void parameterizedTest(int value, String expected) {
+		Assertions.assertEquals(expected,fizzBuzz.compute(value));
 	}
 
 }
